7c55cf056732221c1e1e9b1c04790396689eb4dd
Thomas Vansprengel <iamthadjawi@gmail.com>
2018-12-13 13:24:56 +0100

    Titel

--- app/src/main/java/be/thomasmore/project1_app5v1/Project1_App5v1.java
+++ app/src/main/java/be/thomasmore/project1_app5v1/Project1_App5v1.java
@@ -17,15 +17,7 @@ public class Project1_App5v1 extends AppCompatActivity {
         setContentView(R.layout.activity_project1__app5v1);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        
     }
 
     @Override
