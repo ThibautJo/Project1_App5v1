fa84e3e96c846c6fefd24dcd593bc53c9c4bd520
Thibaut <thibautjoukes@gmail.com>
2018-12-17 11:12:00 +0100

    Oefening 2 almost done

--- app/src/main/java/be/thomasmore/project1_app5v1/Oefening2.java
+++ app/src/main/java/be/thomasmore/project1_app5v1/Oefening2.java
@@ -2,6 +2,7 @@ package be.thomasmore.project1_app5v1;
 
 import android.content.Context;
 import android.content.Intent;
+import android.media.Image;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -9,6 +10,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
 public class Oefening2 extends AppCompatActivity {
 
@@ -30,10 +32,18 @@ public class Oefening2 extends AppCompatActivity {
         // woord ophalen van Oefening 1
         woord = getIntent().getExtras().getString("woord");
 
+        //image
+        int imageFile = getContext().getResources().getIdentifier(woord, "drawable", getContext().getPackageName()); // oef 1 = oef 2 -> zelfde afbeelding
+        ImageView imageView = (ImageView) findViewById(R.id.oef2Foto);
+        imageView.setImageResource(imageFile);
+
+        //audio
         int audioFile = getContext().getResources().getIdentifier("oef2_"+woord, "raw", getContext().getPackageName());
-        audioPlayer = MediaPlayer.create(this, audioFile); // deze lijn is nog hardcoded...
+        audioPlayer = MediaPlayer.create(this, audioFile);
 
         audioPlayer.start();
+
+
 
     }
 
